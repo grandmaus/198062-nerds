df331fd4f64cf559b4e35719ca120ce7e25f3a6b
andruxa0286 <andruxa0286@gmail.com>
2016-05-06 21:42:05 +0300

    Добавил карту и маркер

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var map;
+var mapCenter = {lat: 59.938808, lng: 30.323099};
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: mapCenter,
+    zoom: 17
+  });
+
+
+
+
+
+var image = "/img/map-marker.png";
+  var beachMarker = new google.maps.Marker({
+    position: {lat: 59.938808, lng: 30.323099},
+    map: map,
+    icon: image
+  });
+    
+}
